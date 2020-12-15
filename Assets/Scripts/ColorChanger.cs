c6f97800193a6d3fea1a18d7201cc7e24c7e5c97
Armin Kemura <52348946+brinobre@users.noreply.github.com>
2020-12-15 17:13:38 +0100

    updated menu

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    public Material greyMaterial = null;
+    public Material blueMaterial = null;
+
+    private MeshRenderer meshRenderer = null;
+    private XRBaseInteractable hoverInteractor = null;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        hoverInteractor = GetComponent<XRBaseInteractable>();
+
+        hoverInteractor.onHoverEntered.AddListener(SetBlue);
+        hoverInteractor.onHoverExited.AddListener(SetGrey);
+    }
+
+    private void OnDestroy()
+    {
+        hoverInteractor.onHoverEntered.RemoveListener(SetBlue);
+        hoverInteractor.onHoverExited.RemoveListener(SetGrey);
+    }
+
+    private void SetGrey(XRBaseInteractor interactor)
+    {
+        meshRenderer.material = greyMaterial;
+    }
+
+    private void SetBlue(XRBaseInteractor interactor)
+    {
+        meshRenderer.material = blueMaterial;
+    }
+}
---
+++ Assets/Scripts/HandHider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandHider : MonoBehaviour
+{
+    public SkinnedMeshRenderer meshRenderer;
+    private XRDirectInteractor interactor = null;
+
+    private void Awake()
+    {
+        //meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        interactor = GetComponent<XRDirectInteractor>();
+
+        interactor.onHoverEntered.AddListener(Hide);
+        interactor.onHoverExited.AddListener(Show);
+    }
+
+    private void OnDestroy()
+    {
+        interactor.onHoverEntered.RemoveListener(Hide);
+        interactor.onHoverExited.RemoveListener(Show);
+    }
+
+    private void Show(XRBaseInteractable interactable)
+    {
+        meshRenderer.enabled = true;
+    }
+
+    private void Hide(XRBaseInteractable interactable)
+    {
+        meshRenderer.enabled = false;
+        Debug.Log("working");
+    }
+}
